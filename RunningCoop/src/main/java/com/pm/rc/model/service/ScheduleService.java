d8335fededbb57ad9dfc2375646238bece3d787a
wony8 <wony8@192.168.1.16>
2017-04-12 09:30:54 +0900

    Service 인터페이스 작업

--- RunningCoop/src/main/java/com/pm/rc/model/service/ScheduleService.java
+++ RunningCoop/src/main/java/com/pm/rc/model/service/ScheduleService.java
@@ -1,9 +1,13 @@
 package com.pm.rc.model.service;
 
+import java.util.List;
+
+import com.pm.rc.dto.ScheduleDto;
+
 //일정 관련 기능
 public interface ScheduleService {
 
-	//개인 일정 조회
-	
+	//팀 일정 조회
+	public List<ScheduleDto> teamSchSelect(String pr_id);
 	
 }
